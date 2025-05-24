9fe0b5f1cc382bf3829e65131f35b538ccceabfb
Tesfalem <tesfalemabera2@gmail.com>
2025-05-24 15:47:17 +0300

    Add 1-multi_lnguages

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,4 +1,6 @@
-const message = "C is Fun";
-const message = "Python is cold";
-const message = "JavaScript is amazing";
-console.log(message);
+const message1 = "C is Fun";
+const message2 = "Python is cold";
+const message3 = "JavaScript is amazing";
+console.log(message1);
+console.log(message2);
+console.log(message3);
